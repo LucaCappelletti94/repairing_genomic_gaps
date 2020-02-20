b7fa405987693c47c8c64e5fbfcd1640bff92fe0
zom <t.fontana@net2action.com>
2020-02-20 23:03:30 +0100

    fixed import, again

--- repairing_genomic_gaps/__init__.py
+++ repairing_genomic_gaps/__init__.py
@@ -1,7 +1,8 @@
-from .build_dataset import build_dataset
+from .build_dataset import build_dataset, build_dataset_single
 from .build_denoiser import build_denoiser
 
 __all__ = [
     "build_dataset",
-    "build_denoiser"
+    "build_denoiser",
+    "build_dataset_single"
 ]
