30c80dafc0e1407153ce8df9255ecfcdfbeb98f7
zom <t.fontana@net2action.com>
2020-02-22 15:46:11 +0100

    fixed imports

--- repairing_genomic_gaps/models/autoencoder_model.py
+++ repairing_genomic_gaps/models/autoencoder_model.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Flatten, Dense, Reshape, Conv2DTranspose
-from .utils import axis_softmax, axis_categorical
+from ..utils import axis_softmax, axis_categorical
 from .autoencoder_blocks import encoder_blocks, decoder_blocks
 
 
