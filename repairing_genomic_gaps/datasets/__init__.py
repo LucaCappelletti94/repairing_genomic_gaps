8943378d92fc9b89faac87fc653345b04485f12c
zom <t.fontana@net2action.com>
2020-02-22 15:22:15 +0100

    added multivariate datasets

--- repairing_genomic_gaps/datasets/__init__.py
+++ repairing_genomic_gaps/datasets/__init__.py
@@ -1,5 +1,7 @@
 from .build_synthetic_dataset import build_synthetic_dataset_cae, build_synthetic_dataset_cnn
+from .build_multivariate_synthetic_dataset import build_multivariate_dataset_cae, build_multivariate_dataset_cnn
 
 __all__ = [
-    "build_synthetic_dataset_cae", "build_synthetic_dataset_cnn"
+    "build_synthetic_dataset_cae", "build_synthetic_dataset_cnn",
+    "build_multivariate_dataset_cae", "build_multivariate_dataset_cnn"
 ]
