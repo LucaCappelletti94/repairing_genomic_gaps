298cc8b8058aaf38313d5d1e9155951053497bc0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-02-27 20:35:05 +0000

    Refined models

--- run_cnn_1000.py
+++ run_cnn_1000.py
@@ -6,5 +6,5 @@ from repairing_genomic_gaps import cnn_1000, build_synthetic_dataset_cnn, train_
 if __name__ == "__main__":
     with Notipy():
         model = cnn_1000()
-        train, test = build_synthetic_dataset_cnn(1000, batch_size=512)
+        train, test = build_synthetic_dataset_cnn(1000)
         model = train_model(model, train, test)
