db274508da0656825083170ab83fde07dada317c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-02-29 10:05:58 +0100

    Increased number of workers and chunking

--- repairing_genomic_gaps/reports/report_utils.py
+++ repairing_genomic_gaps/reports/report_utils.py
@@ -66,8 +66,8 @@ def _report_wrapper(kwargs):
 
 
 def parallelize_report(report: Callable, y_true: np.ndarray, y_pred: np.ndarray):
-    workers = min(10, cpu_count())
-    total = workers*2
+    workers = min(20, cpu_count())
+    total = workers*4
     chunk_size = math.ceil(y_true.shape[0]/total)
     tasks = (
         {
