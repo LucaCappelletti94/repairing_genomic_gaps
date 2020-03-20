b05899db76842ed5ad46a9ebf7d04253650c2d13
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-20 22:40:03 +0100

    Retring

--- run_weighted_cae_pipeline.py
+++ run_weighted_cae_pipeline.py
@@ -1,23 +1,8 @@
 from repairing_genomic_gaps import cae_1000, cae_500, cae_200
 from repairing_genomic_gaps import build_multivariate_dataset_cae, build_synthetic_dataset_cae
 from repairing_genomic_gaps import train_model
-
-import multiprocessing as mp
+import os
 from notipy_me import Notipy
-from itertools import product
-
-class NoDaemonProcess(mp.Process):
-    # make 'daemon' attribute always return False
-    def _get_daemon(self):
-        return False
-    def _set_daemon(self, value):
-        pass
-    daemon = property(_get_daemon, _set_daemon)
-
-# We sub-class mp.pool.Pool instead of mp.Pool
-# because the latter is only a wrapper function, not a proper class.
-class MyPool(mp.pool.Pool):
-    Process = NoDaemonProcess
 
 models = [
     (cae_1000, 1000),
@@ -31,31 +16,28 @@ weights = [
 ]
 
 
-def executor(build_model, window_size, weight):
-    datasets = [
-        (build_multivariate_dataset_cae, "multivariate_gaps"),
-        (build_synthetic_dataset_cae, "single_gap"),
-    ]
-    for dataset, dataset_name in datasets:
-        with Notipy(
-            task_name="Model {model_name} trained on {dataset_name} with max weight {weight}".format(
-                model_name=build_model.__name__,
-                dataset_name=dataset_name,
-                weight=weight,
-            )
-        ):
-            model = build_model(
-                use_weighted=True,
-                _max = weight,
-            )
-            train, test = dataset(window_size)
-            path = "{}_with_weight_{}".format(dataset_name, weight)
-            model = train_model(model, train, test, path=path)
-
-tasks = [
-    (*model, weight)
-    for model, weight in product(models, weights)
+datasets = [
+    (build_multivariate_dataset_cae, "multivariate_gaps"),
+    (build_synthetic_dataset_cae, "single_gap"),
 ]
 
-with MyPool(4) as p:
-    p.starmap(executor, tasks)
+for dataset, dataset_name in datasets:
+    for weight in weights:
+        path = "{}_with_weight_{}".format(dataset_name, weight)
+        if os.path.exists(path):
+            continue
+        for build_model, window_size in models:
+            with Notipy(
+                task_name="Model {model_name} trained on {dataset_name} with max weight {weight}".format(
+                    model_name=build_model.__name__,
+                    dataset_name=dataset_name,
+                    weight=weight,
+                )
+            ):
+
+                model = build_model(
+                    use_weighted=True,
+                    _max = weight,
+                )
+                train, test = dataset(window_size)
+                model = train_model(model, train, test, path=path)
